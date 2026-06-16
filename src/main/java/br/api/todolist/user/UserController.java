abfe2495c386e375579b61d7ad2a3e2404b18b7b
andersonsimplicio <anderson25eu@gmail.com>
2026-06-16 02:02:49 -0300

    Tabela de Task

--- src/main/java/br/api/todolist/user/UserController.java
+++ src/main/java/br/api/todolist/user/UserController.java
@@ -1,17 +1,38 @@
 package br.api.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/create")   
-    public void create(@RequestBody UserModel user){
-        System.out.println(user.getUsername());
+    public ResponseEntity create(@RequestBody UserModel user){
+        UserModel userCreated = null;
+        var userIsExistis = this.userRepository.findByUsername(user.getUsername());
+        if(userIsExistis!=null){
+            System.out.println("Usuário já existe!");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }else{
+            
+            var password = BCrypt.withDefaults().hashToString(12,user.getPassword().toCharArray());
+
+            user.setPassword(password);
+            userCreated = this.userRepository.save(user);
+           return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+        }
+
+
+        
     }
 }
--- src/main/java/br/api/todolist/user/UserModel.java
+++ src/main/java/br/api/todolist/user/UserModel.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -16,9 +17,10 @@ public class UserModel {
     @Id
     @GeneratedValue(generator="UUID")
     private UUID id;
+    @Column(unique=true)
     private String username;
     private String name;
-    private String passwd;
+    private String password;
     @CreationTimestamp()
     private LocalDateTime createdAt;
    
